3993c6f8216d1c9820c3a951a2255a351063d290
Anand Singh <pb11aps97@gmail.com>
2026-01-28 17:59:25 -0500

    vite updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,20 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-
-export default defineConfig({
-  plugins: [react()],
-  base:"/AnandSingh-Portfolio",
-
-});
-// https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => ({
-//   server: {
-//     host: "::",
-//     port: 8080,
-//   },
-//   plugins: [
-//     react(),
-//     mode === 'development' &&
-//     componentTagger(),
-//   ].filter(Boolean),
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// }));
+export default defineConfig(({ mode }) => ({
+  base: "/AnandSingh-Portfolio",
+  plugins: [
+    react(),
+    // Enable componentTagger only in development
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // <-- THIS IS KEY
+    },
+  },
+  server: {
+    host: "::",
+    port: 8080,
+  },
+}));
